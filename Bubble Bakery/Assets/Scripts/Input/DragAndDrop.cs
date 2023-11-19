d2584fc1053a4586045e5be42cdef11be25f4447
gringames <grin.games@proton.me>
2023-11-19 11:28:06 +0100

    add draggable objects

---
+++ Bubble Bakery/Assets/Scripts/Input/DragAndDrop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Input
+{
+    public class DragAndDrop : MonoBehaviour
+    {
+        private Vector3 _dragOffset;
+
+        private void OnMouseDown()
+        {
+            _dragOffset = transform.position - GetMousePos();
+        }
+
+        private void OnMouseDrag()
+        {
+            transform.position = GetMousePos() + _dragOffset;
+        }
+
+        private Vector3 GetMousePos()
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(UnityEngine.Input.mousePosition);
+            mousePos.z = 0;
+            return mousePos;
+        }
+    }
+}
