071e9859e44e03c1f0bff1e2117e77f2c0245095
gringames <grin.games@proton.me>
2023-11-30 10:07:31 +0100

    create test scene, character script skeleton and character prefabs

---
+++ Bubble Bakery/Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,49 @@
+﻿using Timeline;
+using UnityEngine;
+
+namespace Characters
+{
+    public class Character : MonoBehaviour
+    {
+        [SerializeField] private string charName;
+
+        [Header("Sounds")] [SerializeField] private AudioClip signatureSound;
+        [SerializeField] private AudioClip happySound;
+        [SerializeField] private AudioClip grumpySound;
+
+        [Header("Actions")] [SerializeField] private MoveAction moveAction;
+        [SerializeField] private TalkAction talkAction;
+        [SerializeField] private OrderAction orderAction;
+
+        public void Move()
+        {
+            if (moveAction is null)
+            {
+                Debug.LogError($"Character {charName} has no MoveAction assigned, therefore it cannot move!");
+                return;
+            }
+
+            // TODO: process ENTER and EXIT correctly
+            moveAction.Handle(null);
+        }
+
+        public void Talk()
+        {
+            if (talkAction is null)
+            {
+                Debug.LogError($"Character {charName} has no TalkAction assigned, therefore it cannot say anything!");
+                return;
+            }
+        }
+
+        public void Order()
+        {
+            if (orderAction is null)
+            {
+                Debug.LogError(
+                    $"Character {charName} has no OrderAction assigned, therefore it cannot order anything!");
+                return;
+            }
+        }
+    }
+}
