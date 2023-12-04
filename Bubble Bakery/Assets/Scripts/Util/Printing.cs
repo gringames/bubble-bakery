f2be74bcc7dfc14733e2a177fae1ffaf96af2624
gringames <grin.games@proton.me>
2023-12-04 19:42:56 +0100

    add utli functions

---
+++ Bubble Bakery/Assets/Scripts/Util/Printing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Util
+{
+    public static class Printing
+    {
+        public static void Print<T>(this IEnumerable<T> enumerable, string name = "list")
+        {
+            var list = enumerable.ToList();
+
+            if (list.Count == 0)
+            {
+                Debug.Log($"{name}: {{ }}");
+                return;
+            }
+
+            StringBuilder s = new($"{name}: {{ ");
+
+            foreach (var element in list) s.Append($"{element}, ");
+
+            // get rid of trailing ", "
+            s.Length -= 2;
+
+            s.Append(" }");
+            Debug.Log(s);
+        }
+    }
+}
