e77bda45d79c7afdf085fe1a03a6d02b8b367b6f
gringames <grin.games@proton.me>
2023-12-04 19:43:07 +0100

    create character map singleton

---
+++ Bubble Bakery/Assets/Scripts/Characters/CharacterMap.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+namespace Characters
+{
+    public class CharacterMap : MonoBehaviour
+    {
+        public static CharacterMap Instance;
+
+        [SerializeField] private Character[] _characters;
+
+        private Dictionary<string, Character> _characterMap;
+
+        private void Awake()
+        {
+            CheckInstance();
+            InitCharacterMap();
+            
+            _characterMap.Print("characters");
+        }
+
+        private void CheckInstance()
+        {
+            if (ExistsOtherInstanceAlready())
+            {
+                Destroy(this);
+                return;
+            }
+
+            Instance = this;
+        }
+
+        private bool ExistsOtherInstanceAlready()
+        {
+            return Instance != null && Instance != this;
+        }
+
+        private void InitCharacterMap()
+        {
+            _characterMap = new Dictionary<string, Character>();
+
+            foreach (var character in _characters)
+            {
+                _characterMap[character.name] = character;
+            }
+        }
+    }
+}
